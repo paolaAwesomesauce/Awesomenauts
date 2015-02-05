d7f366e3727a0be2347becef41984a336c8000c2
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-02-04 16:28:09 -0800

    video 19 Adding Creeps on a timer code and comments

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -256,7 +256,7 @@ game.EnemyCreep = me.Entity.extend({
 		this.alwaysUpdate = true;
 
 		// sets were player is located 
-		this.setVelocity(3, 20);
+		this.body.setVelocity(3, 20);
 
 		// sets type of player
 		this.type = "EnemyCreep";
@@ -270,5 +270,27 @@ game.EnemyCreep = me.Entity.extend({
 
 	}
 	
+
+});
+
+game.GameManager = Object.extend({
+	init: function(x, y, settings){
+		this.now = new Date().getTime();
+		this.lastCreep = new Date().getTime();
+
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		this.now = new Date().getTime();
+
+		if (Math.round(this.now/1000)%10 === 0 && (this.now - this.lastCreep >= 1000)) {
+			this.lastCreep = this.now;
+			var creepe = me.pool.pull("EnemyCreep", 1000, 0 , {});
+			me.game.world.addChild(creepe, 5);
+		}
+
+		return true;
+	}
 
 });
--- js/game.js
+++ js/game.js
@@ -39,11 +39,12 @@ var game = {
 
 	// Run on game resources loaded.
 	"loaded" : function () {
-		//loads player,towers, and enemy players
+		//loads player,towers,enemy players, and game manager timer
 		me.pool.register("player", game.PlayerEntity, true);
 		me.pool.register("PlayerBase", game.PlayerBaseEntity);
 		me.pool.register("EnemyBase", game.EnemyBaseEntity);
 		me.pool.register("EnemyCreep", game.EnemyCreep, true);
+		me.pool.register("GameManager", game.GameManager);
 		
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
