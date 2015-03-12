ed170ce313b043a08b1a59ea4cf6df84620fb150
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-03-12 11:14:13 -0700

    video 45 inceased gold production code and comments

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -10,6 +10,7 @@ game.SpendExp = me.ScreenObject.extend({
 		me.input.bindKey(me.input.KEY.F3, "F3");
 		me.input.bindKey(me.input.KEY.F4, "F4");
 		me.input.bindKey(me.input.KEY.F5, "F5");
+		var exp1cost = ((game.data.exp1 + 1) * 10);
 
 		// put the word awesomenauts! on title screeen 
 		me.game.world.addChild(new (me.Renderable.extend({
@@ -21,7 +22,7 @@ game.SpendExp = me.ScreenObject.extend({
 			draw: function(renderer){
 				this.font.draw(renderer.getContext(), "PRESS F1-F4  TO BUY, F3 TO SKIP", this.pos.x, this.pos.y);
 				this.font.draw(renderer.getContext(), "CURRENT EXP: " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
-				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION CURRENT LEVEL: " + game.data.exp1.toString() + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION CURRENT LEVEL: " + game.data.exp1.toString() + exp1cost, this.pos.x, this.pos.y + 100);
 				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD ", this.pos.x , this.pos.y + 150);
 				this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE ", this.pos.x , this.pos.y + 200);
 				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH ", this.pos.x , this.pos.y + 250);
@@ -31,7 +32,11 @@ game.SpendExp = me.ScreenObject.extend({
 
 			this.handler = me.event.suscribe(me.event.KEYDOWN, function (action, keyCode, edge){
 				if (action === "F1") {
-
+					if (game.data.exp >= exp1cost) {
+						game.data.exp1 += 1;
+						game.data.exp -= exp1cost;
+						me.state.change(me.state.PLAY);
+					}
 				}
 				else if (action === "F2") {
 
