6267259a6a6fccab9db308899996741dddaf685a
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-03-12 15:13:55 -0700

    video 47 Drawing spendGold image code and comments

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -99,9 +99,40 @@ game.SpendGold = Object.extend({
 		this.lastBuy = new Date().getTime();
 		this.paused = false;
 		this.alwaysUpdate = true;
+		this.updateWhenPaused = true;
+		this.buying = false;
 	},
 
 	update: function(){
+		this.now = new Date().getTime();
+
+		if (me..input.isKeyPressed("buy") && this.now-this.lastBuy >=1000) {
+			this.lastBuy = this.now;
+			if (!this.buying) {
+				this.startBuying();
+			}
+			else{
+				this.stopBullying();
+			}
+		}
 		return true;
+	},
+
+	startBuying: function(){
+		this.buying = true;
+		me.state.pause(me.state.PLAY);
+		game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+		game.data.buySscren = new me.Sprite(game.data.pausedPos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
+		game.data.buyscreen.updateWhenPaused = true;
+		game.data.buyscreen.setOpacity(0.8);
+		me.game.world.addChild(game.data.buyscreen, 34);
+		game.data.player.body.setVelocity(0, 0);
+	},
+
+	stopBuying: function(){
+		this.buying = false;
+		me.state.resume(me.state.PLAY);
+		game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+		me.game.world.removeChild(game.data.buyscreen);
 	}
 });
