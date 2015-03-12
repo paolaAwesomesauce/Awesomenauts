b00be3a80170274d07070c55435072c3a61a2126
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-03-12 10:14:35 -0700

    video 44 Finishing SpendExp code and comments

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -5,24 +5,49 @@ game.SpendExp = me.ScreenObject.extend({
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
 
+		me.input.bindKey(me.input.KEY.F1, "F1");
+		me.input.bindKey(me.input.KEY.F2, "F2");
+		me.input.bindKey(me.input.KEY.F3, "F3");
+		me.input.bindKey(me.input.KEY.F4, "F4");
+		me.input.bindKey(me.input.KEY.F5, "F5");
+
 		// put the word awesomenauts! on title screeen 
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
 				this._super(me.Renderable, 'init', [10, 10, 300, 50]);
-				this.font = new me.Font("Arial", 46, "white");
+				this.font = new me.Font("Arial", 26, "white");
 			},
 
 			draw: function(renderer){
 				this.font.draw(renderer.getContext(), "PRESS F1-F4  TO BUY, F3 TO SKIP", this.pos.x, this.pos.y);
 				this.font.draw(renderer.getContext(), "CURRENT EXP: " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
-				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 100);
-				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 150);
-				this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 200);
-				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 250);
+				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION CURRENT LEVEL: " + game.data.exp1.toString() + ((game.data.exp1 + 1) * 10), this.pos.x, this.pos.y + 100);
+				this.font.draw(renderer.getContext(), "F2: ADD STARTING GOLD ", this.pos.x , this.pos.y + 150);
+				this.font.draw(renderer.getContext(), "F3: INCREASE ATTACK DAMAGE ", this.pos.x , this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH ", this.pos.x , this.pos.y + 250);
 			}
 
 			})));
 
+			this.handler = me.event.suscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+				if (action === "F1") {
+
+				}
+				else if (action === "F2") {
+
+				}
+				else if (action === "F3") {
+					
+				}
+				else if (action === "F4") {
+					
+				}
+				else if (action === "F5") {
+					me.state.change(me.state.PLAY);
+				}
+
+			});
+			
 	},
 	
 	
@@ -32,6 +57,12 @@ game.SpendExp = me.ScreenObject.extend({
 
 	 // unbinds the enter key so when we press it wont send us to the beginning thing
 	onDestroyEvent: function() {
+		me.input.unbindKey(me.input.KEY.F1, "F1");
+		me.input.unbindKey(me.input.KEY.F2, "F2");
+		me.input.unbindKey(me.input.KEY.F3, "F3");
+		me.input.unbindKey(me.input.KEY.F4, "F4");
+		me.input.unbindKey(me.input.KEY.F5, "F5");
+		me.event.unsubscribe(this.handler);
 
 	}
 });
