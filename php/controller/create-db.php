9a4a2bd05f75d668c15306825445a6009c219de9
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-04-01 11:17:17 -0700

    video 55 Finishing PHP code

--- php/controller/create-db.php
+++ php/controller/create-db.php
@@ -3,22 +3,6 @@
 //Looks for config.php where we refactored the code
 require_once(__DIR__ . "/../model/config.php");
 
-	//Creates datatable for posts that have a unique id. Our post are text and none of them can be null.
-	$query = $_SESSION["connection"]->query("CREATE TABLE posts ("
-		. "id int(11) NOT NULL AUTO_INCREMENT," 
-		. "title varchar(255) NOT NULL," 
-		. "post text NOT NULL,"
-		. "DateTime datetime NOT NULL ," 
-		. "PRIMARY KEY (id))");
-
-	//Creates table name posts and checks if theres an error in your connection;
-	if($query){
-		//echo "<p>Successfully created table : posts</p>";
-	}
-	else {
-		echo "<p>" . $_SESSION["connection"]->error . "</p>";
-	}
-
 //creates a database for the for registered users
 	$query = $_SESSION["connection"]->query("CREATE TABLE users ("
 		. "id int (11) NOT NULL AUTO_INCREMENT," 
@@ -26,6 +10,11 @@ require_once(__DIR__ . "/../model/config.php");
 		."email varchar (50) NOT NULL,"
 		. "password char(128) NOT NULL,"
 		. "salt char(128) NOT NULL,"
+		. "exp int(4),"
+		. "exp1 int(4),"
+		. "exp2 int(4),"
+		. "exp3 int(4),"
+		. "exp4 int(4),"
 		. "PRIMARY KEY (id))");
 
 // if/else statement checks if we properly created database
