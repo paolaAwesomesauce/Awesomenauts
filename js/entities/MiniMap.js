4a9dc4534239b6612095227270c39cea45ea1a72
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-04-05 17:42:18 -0700

    video 64 displaying minimap

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,3 +1,15 @@
 game.MiniMap = me.Entity.extend({
-
+	init: function(x, y, settings){
+		this._super(me.Entity, "init", [x, y, {
+			image: "minimap",
+			width: 561,
+			height: 316,
+			spritewidth: "561",
+			spriteheight: "316",
+			getShape: function(){
+				return (new me.Rect(0, 0, 561, 316)).toPolygon();
+			}
+		}]);
+		this.floating = true;
+	}
 })
